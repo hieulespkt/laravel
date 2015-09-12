250df095abdfc0c4e489c2a233948bd5a89739e9
Le Nhat Hieu <hieulespkt94@gmail.com>
2015-09-12 21:22:39 +0700

    Initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    public function index()
+    {
+        return view("share.__layout");
+    }
+}
---
+++ app/Http/Controllers/admin/layout.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controller\admin;
+use \Illuminate\Routing\Controller;
+class layout extends Controller
+{
+    public function changelogo()
+    {
+
+    }
+}
